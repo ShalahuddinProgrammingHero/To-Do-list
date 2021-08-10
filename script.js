cbcb0fd88395f9a1a8512974d48f62e37f0fd6f3
shalahuddinProgrammingHero <mohammadshalahuddins@gmail.com>
2021-08-11 01:12:49 +0600

    To Do List Complete With Responsive

---
+++ script.js
@@ -0,0 +1,27 @@
+/* Selector */
+
+// document.getElementById
+// document.getElementsByClassName
+// document.getElementsByName
+// document.querySelector
+// document.querySelectorAll
+// children.closest
+// nextElementSibling
+// previousElementSibling
+
+
+/* Create an Element */
+// createElement
+// className
+// setAttribute()
+// inserBefore
+// appendChild
+// append
+
+
+/* add Event Listeners */
+// addEventListener */ click/dblclick/mousedown/mouseup/mouseenter/mouseleave/mouseover/mouseout/mousemove/
+
+// for input */ keydown/keyup/keypress/focus/blur/cut/paste/input
+
+// for submit */ submit (preventDefaul)/
